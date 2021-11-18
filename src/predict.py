041fcdd72d9d430371ec1aec62f448ece5fed2b2
csand83 <christophe.sand1@free.fr>
2021-11-18 18:48:10 +0100

    Data update by RPi

--- src/predict.py
+++ src/predict.py
@@ -73,7 +73,7 @@ def preprocess_data_for_pred(df):
 def predict_next():
     main_df = construct_df_for_pred(RATIOS)
     preprocessed_sequences = preprocess_data_for_pred(main_df)
-
+    print(preprocessed_sequences)
     #interpreter = tflite.Interpreter(model_path=MODEL_PATH_ABS)
     model = keras.models.load_model(MODEL_PATH_ABS)
     #interpreter.allocate_tensors()
